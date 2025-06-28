d1f4479a6c63b647bbe37ca8b30687c7bb67a2ea
montukharb <montukharb868@gmail.com>
2025-06-29 01:31:52 +0530

    create navbar menu

--- src/app/Header/menu/menu.component.ts
+++ src/app/Header/menu/menu.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
-
+import { menuItems } from './models/menuItems';
 @Component({
   selector: 'app-menu',
   imports: [],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss',
 })
-export class MenuComponent {}
+export class MenuComponent {
+  m_items = menuItems;
+   hoveredIndex:number|null = null 
+  showid(id:number,label:string|object)
+  {
+    this.hoveredIndex = id;
+    console.log("id = ",id," label = ",label);
+  } 
+  hideid(id2:number,label2:string|object)
+  {
+    this.hoveredIndex = null;
+    console.log("hideid called");
+    console.log("id = ",id2," label = ",label2);
+  }
+}
